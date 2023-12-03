66cbf57ff9b77d3f5097f7ddd71baec93a7f68de
sajjad.jalilian <sajjad.jalilian@partsoftware,com>
2023-12-03 23:37:57 +0330

    adding new file for abstraction

--- app/src/main/java/com/example/shakingdetector/MainActivity.kt
+++ app/src/main/java/com/example/shakingdetector/MainActivity.kt
@@ -22,66 +22,22 @@ import com.example.shakingdetector.ui.theme.ShakingDetectorTheme
 import java.util.Objects
 import kotlin.math.sqrt
 
-class MainActivity : ComponentActivity() {
-    private var sensorManager: SensorManager? = null
-    private var acceleration = 0f
-    private var currentAcceleration = 0f
-    private var lastAcceleration = 0f
-    private var lastShakeTime: Long = 0
+class MainActivity : ComponentActivity(), ShakeDetector.OnShakeListener {
+    private lateinit var shakeDetector: ShakeDetector
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setcontent{
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        Objects.requireNonNull(sensorManager)?.registerListener(
-            sensorListener, sensorManager!!
-                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL
-        )
-        acceleration = 1f
-        currentAcceleration = SensorManager.GRAVITY_EARTH //standard acceleration
-        lastAcceleration = SensorManager.GRAVITY_EARTH //baseline for calculating changes
-        lastShakeTime = System.currentTimeMillis()
-
+        shakeDetector = ShakeDetector(this, this)
     }
-
-    private val sensorListener: SensorEventListener =
-        object : SensorEventListener { //anonymous object
-            override fun onSensorChanged(event: SensorEvent) {
-                val x = event.values[0]
-                val y = event.values[1]
-                val z = event.values[2]
-                //measures acceleration force in m/s^2 that is applied to a device on all three physical axes (x, y, and z), including the force of gravity
-                lastAcceleration = currentAcceleration
-                currentAcceleration = sqrt((x * x + y * y + z * z).toDouble()).toFloat()
-                val delta: Float = currentAcceleration - lastAcceleration
-                acceleration =
-                    acceleration * 0.9f + delta //applies a smoothing factor (0.9f) to the existing acceleration and adds the change in acceleration (delta). This is a way to smooth out rapid changes in acceleration and focus on gradual changes
-                val currentTime = System.currentTimeMillis()
-                val timeElapsedSinceLastShake = currentTime - lastShakeTime
-                Log.d(
-                    "SensorManagers",
-                    "onSensorChanged: acceleration: $acceleration, timeElapsedSinceLastShake: $timeElapsedSinceLastShake"
-                )
-                if (acceleration > 3.5 && timeElapsedSinceLastShake > 1000) {
-                    Toast.makeText(this@MainActivity, "Shake detected!", Toast.LENGTH_SHORT).show()
-                    lastShakeTime = currentTime
-                }
-            }
-
-            override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
-        }
-
+    override fun onShake() {
+        Toast.makeText(this, "Shake detected!", Toast.LENGTH_SHORT).show()
+    }
     override fun onResume() {
-        sensorManager?.registerListener(
-            sensorListener, sensorManager!!.getDefaultSensor(
-                Sensor.TYPE_ACCELEROMETER
-            ), SensorManager.SENSOR_DELAY_NORMAL
-        )
         super.onResume()
+        shakeDetector.onResume()
     }
-
     override fun onPause() {
-        sensorManager!!.unregisterListener(sensorListener)
         super.onPause()
+        shakeDetector.onPause()
     }
 }
 
---
+++ app/src/main/java/com/example/shakingdetector/ShakeDetector.kt
@@ -0,0 +1,74 @@
+package com.example.shakingdetector
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.util.Log
+import java.util.Objects
+import kotlin.math.sqrt
+
+class ShakeDetector(private val context: Context, private val onShakeListener: OnShakeListener) :
+    SensorEventListener {
+
+    interface OnShakeListener {
+        fun onShake()
+    }
+
+    private var sensorManager: SensorManager? = null
+    private var acceleration = -10f
+    private var currentAcceleration = 0f
+    private var lastAcceleration = 0f
+    private var lastShakeTime: Long = 0
+
+
+    init {
+        initializeSensorManager()
+    }
+
+    private fun initializeSensorManager() {
+        sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        Objects.requireNonNull(sensorManager)?.registerListener(
+            this,
+            sensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        val x = event.values[0]
+        val y = event.values[1]
+        val z = event.values[2]
+
+        lastAcceleration = currentAcceleration
+        currentAcceleration = sqrt((x * x + y * y + z * z).toDouble()).toFloat()
+
+        val delta: Float = currentAcceleration - lastAcceleration
+        acceleration = acceleration * 0.9f + delta
+
+        val currentTime = System.currentTimeMillis()
+        val timeElapsedSinceLastShake = currentTime - lastShakeTime
+        Log.d("ShakeDetector", "acceleration: $acceleration")
+        if (acceleration > 4.5 && timeElapsedSinceLastShake > 1000) {
+            onShakeListener.onShake()
+            lastShakeTime = currentTime
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+        // Ignore
+    }
+
+    fun onResume() {
+        sensorManager?.registerListener(
+            this,
+            sensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
+
+    fun onPause() {
+        sensorManager?.unregisterListener(this)
+    }
+}
